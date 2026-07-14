6255003cd9edf16e18ff0f62bc1488d5ce0effca
jekidev <jeki_dev@proton.me>
2026-07-14 13:07:35 +0200

    Simplify router package exports

--- llm_router/__init__.py
+++ llm_router/__init__.py
@@ -1,7 +1,11 @@
 from .client import OpenRouterClient, LLMResponse
-from .sync import SyncOpenRouterClient
 from .config import RouterConfig
-from .catalog import ModelCatalog
-from .models import ModelInfo
 from .errors import LLMRouterError, AllRoutesFailed
-__all__ = ["OpenRouterClient", "SyncOpenRouterClient", "LLMResponse", "RouterConfig", "ModelCatalog", "ModelInfo", "LLMRouterError", "AllRoutesFailed"]
+
+__all__ = [
+    "OpenRouterClient",
+    "LLMResponse",
+    "RouterConfig",
+    "LLMRouterError",
+    "AllRoutesFailed",
+]
